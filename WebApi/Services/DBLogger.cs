a028df13a8a7b77e3289815255a2ad8db16a5ddf
Toprak Tüzün <topraktuzun17@hotmail.com>
2022-06-28 16:35:12 +0300

    add logger service

---
+++ WebApi/Services/DBLogger.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace WebApi.Services
+{
+    public class DBLogger : ILoggerService
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine("[DBLogger] - " + message);
+        }
+
+    } 
+}
